7d537d31f2de4e7eba0e115990802a17fb4e40d9
Francis Tan <ftan@localhost.localdomain>
2016-01-16 20:46:45 -0800

    Added fedora based installation

---
+++ install_fedora.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo dnf -y install zsh emacs tree multitail gitflow
+rm ~/.zshrc
+ln -s ~/.environment_settings/.emacs ~/.emacs
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+rm get-pip.py
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+ln -s ~/.environment_settings/toothed.zsh-theme ~/.oh-my-zsh/themes/toothed.zsh-theme
+ln -s ~/.environment_settings/multitailrc ~/.multitailrc
+ln -s ~/.environment_settings/.zshrc ~/.zshrc
+sudo chsh -s $(which zsh)
