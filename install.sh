8e63aacf911dcbb8230e900bb2cc334025ca6fa9
Francis Tan <francis.adrian.tan@gmail.com>
2016-01-17 21:59:04 -0800

    Moved symlink down

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get -y install zsh emacs24 tree multitail git-flow
 rm ~/.zshrc
-ln -s ~/.environment_settings/.zshrc ~/.zshrc
 ln -s ~/.environment_settings/.emacs ~/.emacs
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
@@ -12,4 +11,5 @@ sudo pip install virtualenvwrapper
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -s ~/.environment_settings/toothed.zsh-theme ~/.oh-my-zsh/themes/toothed.zsh-theme
 ln -s ~/.environment_settings/multitailrc ~/.multitailrc
+ln -s ~/.environment_settings/.zshrc ~/.zshrc
 sudo chsh -s $(which zsh)
